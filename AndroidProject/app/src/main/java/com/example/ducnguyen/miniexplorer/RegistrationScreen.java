4c1aab34af8f167c8a50737f38f2d3dfd1825b2e
ngtrangminhduc <ngtrangminhduc@gmail.com>
2018-10-16 23:34:56 -0400

    Create quit button
    on registration screen

--- AndroidProject/app/src/main/java/com/example/ducnguyen/miniexplorer/RegistrationScreen.java
+++ AndroidProject/app/src/main/java/com/example/ducnguyen/miniexplorer/RegistrationScreen.java
@@ -2,6 +2,8 @@ package com.example.ducnguyen.miniexplorer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class RegistrationScreen extends AppCompatActivity {
 
@@ -10,4 +12,20 @@ public class RegistrationScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_screen);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.user){
+            moveTaskToBack(true);
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(1);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
