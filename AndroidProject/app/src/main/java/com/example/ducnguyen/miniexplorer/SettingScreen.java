145e6e5a0827558591c3c6816880cc8fd5b102e5
ngtrangminhduc <ngtrangminhduc@gmail.com>
2018-10-17 15:26:01 -0400

    PRESENTATION MILESTONE

--- AndroidProject/app/src/main/java/com/example/ducnguyen/miniexplorer/SettingScreen.java
+++ AndroidProject/app/src/main/java/com/example/ducnguyen/miniexplorer/SettingScreen.java
@@ -1,7 +1,9 @@
 package com.example.ducnguyen.miniexplorer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingScreen extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class SettingScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_screen);
+    }
+    public void gotoMainMenu (View view)
+    {
+        Intent intent = new Intent(SettingScreen.this, MainMenu.class);
+        startActivity(intent);
     }
 }
