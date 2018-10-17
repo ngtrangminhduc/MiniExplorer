0c4100ea3b48a603ffce73eaa5cd7aeacd31a156
ngtrangminhduc <ngtrangminhduc@gmail.com>
2018-10-17 14:07:53 -0400

    Finish ReadingScreen

--- AndroidProject/app/src/main/java/com/example/ducnguyen/miniexplorer/ReadingScreen.java
+++ AndroidProject/app/src/main/java/com/example/ducnguyen/miniexplorer/ReadingScreen.java
@@ -1,7 +1,9 @@
 package com.example.ducnguyen.miniexplorer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ReadingScreen extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ReadingScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reading_screen);
+    }
+    public void gotoMainMenu (View view)
+    {
+        Intent intent = new Intent(ReadingScreen.this, MainMenu.class);
+        startActivity(intent);
     }
 }
