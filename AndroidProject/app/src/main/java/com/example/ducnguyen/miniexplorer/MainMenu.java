ec849cb212ad251892198e13d739cafa18f14cea
ngtrangminhduc <ngtrangminhduc@gmail.com>
2018-10-17 12:03:49 -0400

    Create quit button on MainMenu

--- AndroidProject/app/src/main/java/com/example/ducnguyen/miniexplorer/MainMenu.java
+++ AndroidProject/app/src/main/java/com/example/ducnguyen/miniexplorer/MainMenu.java
@@ -2,6 +2,10 @@ package com.example.ducnguyen.miniexplorer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.content.Intent;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.user){
+            moveTaskToBack(true);
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(1);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
